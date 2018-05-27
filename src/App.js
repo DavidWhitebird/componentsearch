59db1f5f71150d946bddffcfa5106b2cde7a31fc
dw <davidwhitebird@gmail.com>
2018-05-27 03:34:40 -0500

    Header footer action

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+"we're going to try to have states reside in the app component."
+
+
+
 class App extends Component {
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import PApp from './Components/PApp';
 import registerServiceWorker from './registerServiceWorker';
+import 'typeface-roboto';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<PApp />, document.getElementById('root'));
 registerServiceWorker();
