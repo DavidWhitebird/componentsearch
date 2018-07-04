4ff43589d9e38f972557c8887c7164c2c34ddd14
dw <davidwhitebird@gmail.com>
2018-07-03 20:04:56 -0500

    menu opens

--- src/Components/Layouts/Body.js
+++ src/Components/Layouts/Body.js
@@ -6,6 +6,22 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Button from '@material-ui/core/Button';
+
+
+
 const styles = theme => ({
     root: {
         width: '50%',
@@ -15,35 +31,83 @@ const styles = theme => ({
 });
 
 
-function ListDividers(props) {
+class ListDividers extends React.Component {
+    state = {
+        auth: true,
+        anchorEl: null,
+    };
+
+    handleMenu2 = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+    handleClose2 = () => {
+        this.setState({anchorEl: null});
+    };
+
+    handleChange = (event, checked) => {
+        this.setState({ auth: checked });
+    };
+
+    handleMenu = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
 
     handleClose = () => {
         this.setState({ anchorEl: null });
     };
 
-    const { classes } = props;
-    return (
-        <div className={classes.root}>
-            <List component="nav">
-                <ListItem button>
-                    <ListItemText primary="1" />
-                </ListItem>
-                <Divider />
-                <ListItem button divider>
-                    <ListItemText primary="2" />
-                </ListItem>
-                <ListItem button>
-                    <ListItemText onClick={this.handleclose} />
-                    3
-                </ListItem>
-                <Divider light />
-                <ListItem button>
-                    <ListItemText primary="Create New Playlist" />
+    render() {
+        const { classes } = this.props;
+        const { auth, anchorEl } = this.state;
+        const open = Boolean(anchorEl);
+        return (
+            <div className={classes.root}>
+                <List component="nav">
+                    <ListItem button>
+                        <ListItemText primary="1"/>
+                    </ListItem>
+                    <Divider/>
+                    <ListItem button divider>
+                        <ListItemText primary="2"/>
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemText onClick={this.handleClose2}/>
+                        3
+                    </ListItem>
+                    <Divider light/>
+                    <ListItem button>
+                        <ListItemText
+                            primary="Create New Playlist"
+                            aria-owns={open ? 'menu-appbar' : null}
+                            aria-haspopup="true"
+                            onClick={this.handleMenu}
+                            color="inherit"
 
-                </ListItem>
-            </List>
-        </div>
-    );
+
+                        />
+                        <Menu
+                            id="User-Menu"
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={open}
+                            onClose={this.handleClose}
+                        >
+                            <MenuItem onClick={this.handleClose}>Playlists</MenuItem>
+                            <MenuItem onClick={this.handleClose}>Create New Playlist</MenuItem>
+                        </Menu>
+
+                    </ListItem>
+                </List>
+            </div>
+        );
+    }
 }
 
 ListDividers.propTypes = {
