5098d55208ad98445e85fb91eed52acc32d7df80
dw <davidwhitebird@gmail.com>
2018-07-07 12:12:56 -0500

    readme

--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -10,9 +10,9 @@
 //    #####     ADD VALUES TO THESE VARIABLES     #####
 //        DO NOT UPLOAD ACTUAL VALUES TO GITHUB
 
-var client_id = 'de11a78ad2b94a5094ee825ef4088a5e'; // Your client id
-var client_secret = '53f120e7b4c344289c41f89620d6ea93'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var client_id = 'VALUEGOESHERE'; // Your client id
+var client_secret = 'VALUEGOESHERE'; // Your secret
+var redirect_uri = 'VALUEGOESHERE'; // Your redirect uri
 
 
 //    #####           END                          #####
