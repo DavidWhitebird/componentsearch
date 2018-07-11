dbda7a57b9fba5217b0edc26f1657ff893b8151f
dw <davidwhitebird@gmail.com>
2018-07-11 17:09:19 -0500

    config

---
+++ auth-server/authorization_code/secretsconfig.js
@@ -0,0 +1,29 @@
+var config = {};
+
+config.spotify_client_id = 'de11a78ad2b94a5094ee825ef4088a5e'; // Your client id
+config.spotify_client_secret = '53f120e7b4c344289c41f89620d6ea93'; // Your secret
+config.spotify_redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+config.mongo_uri_command = 'mongodb://bt:U7zv66nTDVJ3w7Ck@ds231951.mlab.com:31951/heroku_dhxhmsfk'; // mongo uri connection command
+config.mongo_url = 'mongodb://bt:U7zv66nTDVJ3w7Ck@ds231951.mlab.com:31951/heroku_dhxhmsfk'; // mongo uri connection command
+config.mongo_shell_connection = 'mongo ds231951.mlab.com:31951/heroku_dhxhmsfk -u bt -p U7zv66nTDVJ3w7Ck';
+config.mongo_user = "heroku_dhxhmsfk";
+config.mongo_account = 'heroku_dhxhmsfk';
+
+
+module.exports = config;
+
+
+
+
+
+/*          EXAMPLE COMMANDS
+mongodb=//bt=U7zv66nTDVJ3w7Ck@ds231951.mlab.com=31951/heroku_dhxhmsfk
+mongo ds231951.mlab.com=31951/heroku_dhxhmsfk -u bt -p U7zv66nTDVJ3w7Ck
+
+* */
+
+
+
+
+
